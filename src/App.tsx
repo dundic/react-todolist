dfded9b86549a5b58b627a118b36dfc8e89936f8
dundic <tadaleck@gmail.com>
2024-10-04 14:05:16 +0300

    start appication

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+    let tasks1: Array<TaskType> = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+    let tasks2: Array<TaskType>  = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'XXX', isDone: false},
+        {id: 3, title: 'Big Nothing', isDone: true}
+    ]
+
+    return (
+        <div className="App">
+            < Todolist title={"What to learn"} tasks={tasks1}/>
+        </div>
+    );
 }
+
 
 export default App;
